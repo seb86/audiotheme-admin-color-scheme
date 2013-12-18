c737b86541e44dea54d9618635001346339f1744
Brady Vercher <brady@blazersix.com>
2013-12-18 15:37:43 -0800

    Move icon desaturation styles to the style sheet.

--- admin-color-schemes.php
+++ admin-color-schemes.php
@@ -25,7 +25,6 @@ class Audiotheme_Color_Schemes {
 	function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'load_default_css' ) );
 		add_action( 'admin_init' , array( $this, 'add_colors' ) );
-		add_action( 'admin_head', array( $this, 'desaturate_menu_image_icons' ) );
 	}
 
 	/**
@@ -59,29 +58,6 @@ class Audiotheme_Color_Schemes {
 			$wp_styles->registered[ 'colors' ]->deps[] = 'colors-fresh';
 		}
 
-	}
-
-	/**
-	 * Print CSS in the admin header to apply grayscale filters to
-	 *
-	 * @link https://gist.github.com/bradyvercher/7947249
-	 */
-	function desaturate_menu_image_icons() {
-		?>
-		<style type="text/css">
-		#adminmenu li.menu-top .wp-menu-image img {
-			filter: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg'><filter id='grayscale'><feColorMatrix type='matrix' values='0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0'/></filter></svg>#grayscale"); /* Firefox 10+, Firefox on Android */
-			filter: gray; /* IE6-9 */
-			-webkit-filter: grayscale(100%); /* Chrome 19+, Safari 6+, Safari 6+ iOS */
-			opacity: 1;
-		}
-
-		#adminmenu li.wp-has-current-submenu .wp-menu-image img {
-			filter: none;
-			-webkit-filter: grayscale(0%);
-		}
-		</style>
-		<?php
 	}
 }
 global $acs_colors;
